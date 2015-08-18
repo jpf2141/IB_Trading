0b1fa8de7876775f7a90449ebf028b107c29d909
joshfram <jpf2141@columbia.edu>
2015-08-18 12:51:29 -0400

    added get stock method

--- src/trading/Trader.java
+++ src/trading/Trader.java
@@ -4,29 +4,39 @@ package trading;
 
 public class Trader { 
 	
-	public static int clientID = 1;
+	public static final int clientID = 1;
+	
 	public Socket socket;
 
 	
 	public Trader() { 
 		socket = new Socket();
-		if(socket.connect(null, 7496, clientID)) {
-			socket.connectionID = clientID;	//associate the client with a client
-			clientID++;		//if another connection is made later, 
-							//clientID must remain unique
+		if(!socket.connect(null, 7496, clientID)) {	//if connection fails
+			return;
 		}
 	}
 	
 	public static void main(String[] args) {
 		Trader trader = new Trader();
-		//System.out.println(trader.socket.connection.isConnected());
+		if(!trader.socket.connection.isConnected()) {
+			return;		//connection failed, return
+		}
 		
-		
+		getStock();
 		
 		
 		trader.socket.disconnect();
 		
 	
+	}
+
+	private static void getStock() {
+		
+		
+		
+		
+		
+		
 	}
 	
 }
